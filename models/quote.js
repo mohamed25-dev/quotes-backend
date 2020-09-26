0d4c39b753394d722a39f6aa6574aa103677aaf1
mohamed <mohammed2571994@gmail.com>
2020-09-26 11:00:09 +0200

    Change quote to TEXT data type

--- models/quote.js
+++ models/quote.js
@@ -9,7 +9,7 @@ module.exports = function(sequelize, DataTypes) {
         autoIncrement: true
       },
       quote: {
-        type: DataTypes.STRING(255),
+        type: DataTypes.TEXT,
         allowNull: false
       },
       categoryId: {
